3fdcc162acc4d1267c6f60cddc5c38642e6270a7
Heneros <punk60826@gmail.com>
2024-03-30 19:29:28 +0200

    docker

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ import cookieParser from "cookie-parser";
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import connectionToDB from "./config/connectDB.js";
 
+import { morganMiddleware, systemLogs } from "./utils/Logger.js";
+import mongoSanitize from "express-mongo-sanitize";
+
+await connectionToDB();
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
@@ -13,6 +18,9 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(mongoSanitize());
+
+app.use(morganMiddleware);
 
 app.get("/api/v1/test", (req, res) => {
   res.json({ Hi: "Hello World" });
@@ -22,4 +30,7 @@ const PORT = process.env.PORT || 1997;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  systemLogs.info(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}    `
+  );
 });
